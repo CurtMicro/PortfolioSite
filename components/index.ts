bf0b872ba62315f4075f75b7ce95d33882319be8
CurtMicro <curtismaple1@hotmail.com>
2023-11-13 22:45:55 -0600

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton"
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchMake from "./SearchMake";
+import ProjectCard from "./ProjectCard";
+import ProjectDetails from "./ProjectDetails";
+
+export {
+    Navbar,
+    Hero,
+    CustomButton,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchMake,
+    ProjectCard,
+    ProjectDetails
+}
